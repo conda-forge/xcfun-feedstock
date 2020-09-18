5bd4e9a79b4c56b1818a3980d8c398489c436b5c
Roberto Di Remigio <roberto.diremigio@gmail.com>
2020-09-18 15:17:28 +0200

    MNT: Re-rendered with conda-build 3.20.2, conda-smithy 3.7.10, and conda-forge-pinning 2020.09.18.08.23.49

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,18 +1,18 @@
 :: configure
 cmake -H"%SRC_DIR%" ^
       -Bbuild ^
-      -G"Ninja" ^
-      -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ^
+      -GNinja ^
+      -DCMAKE_INSTALL_PREFIX="%PREFIX%" ^
       -DCMAKE_BUILD_TYPE=Release ^
-      -DCMAKE_INSTALL_LIBDIR="%LIBRARY_LIB%" ^
-      -DPYMOD_INSTALL_LIBDIR="/../../Lib/site-packages" ^
+      -DCMAKE_INSTALL_LIBDIR="Library\lib" ^
+      -DPYMOD_INSTALL_LIBDIR="..\..\Lib\site-packages" ^
       -DXCFUN_MAX_ORDER=8 ^
       -DXCFUN_PYTHON_INTERFACE=ON ^
-      -DCMAKE_CXX_FLAGS="/wd4018 /wd4101 /wd4996" ^
-      -DCMAKE_PREFIX_PATH="%LIBRARY_PREFIX%" ^
-      -DCMAKE_INSTALL_INCLUDEDIR="%LIBRARY_INC%" ^
-      -DCMAKE_INSTALL_BINDIR="%LIBRARY_BIN%" ^
-      -DCMAKE_INSTALL_DATADIR="%LIBRARY_PREFIX%" ^
+      -DCMAKE_CXX_FLAGS="/wd4018 /wd4101 /wd4996 /EHsc" ^
+      -DCMAKE_INSTALL_INCLUDEDIR="Library\include" ^
+      -DCMAKE_INSTALL_BINDIR="Library\bin" ^
+      -DCMAKE_INSTALL_DATADIR="Library" ^
+      -DDEF_INSTALL_CMAKEDIR="Library\share\cmake\XCFun" ^
       -DCMAKE_WINDOWS_EXPORT_ALL_SYMBOLS=true
 if errorlevel 1 exit 1
 
@@ -20,17 +20,18 @@ if errorlevel 1 exit 1
 cd build
 cmake --build . ^
       --config Release ^
-      -- -j %CPU_COUNT%
+      -- -j %CPU_COUNT% -v -d stats
 if errorlevel 1 exit 1
 
 :: test
 :: The Python interface is tested using pytest directly
-ctest -E "python-interface" --output-on-failure --verbose
+ctest -E "python-interface" ^
+      --output-on-failure ^
+      --verbose
 if errorlevel 1 exit 1
 
 :: install
 cmake --build . ^
       --config Release ^
-      --target install ^
-      -- -j %CPU_COUNT%
+      --target install
 if errorlevel 1 exit 1
